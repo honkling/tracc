f0241d2ccc7e29777bc3bb75be27b175c42170fb
untitledgeese <62254220+untitledgeese@users.noreply.github.com>
2020-11-05 10:21:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const mineflayer = require('mineflayer');
 const Discord = require('discord.js');
 const fs = require('fs');
 const tags = require('./tags.json');
+const envy = require('envy');
+const env = envy();
 const bot = new Discord.Client();
 const a = mineflayer.createBot({
-	host: process.env.SERVER,
-	username: process.env.MINECRAFT_EMAIL,
-	password: process.env.MINECRAFT_PASS
+	host: env.server,
+	username: env.minecraftEmail,
+	password: env.minecraftPass
 });
+
+console.log(env);
 
 a.once('spawn', () => {
 	console.log('Spawned!');
@@ -17,13 +21,19 @@ a.on('message', (msg) => {
 	console.log(msg.toString());
 	let message = msg.toString().replace(/(\`)/g, '');
 	if(msg.toString() === '' || `\`${message}\`` === '``') return;
-	bot.channels.cache.get(process.env.DISCORD_CHANNEL).send(`\`${message}\``);
+	bot.channels.cache.get(env.discordChannel).send(`\`${message}\``);
 });
 
 bot.on('message', (msg) => {
-	if(msg.author.id !== process.env.YOUR_ID) return;
-	if(msg.channel.id === process.env.DISCORD_CHANNEL) {
-		a.chat(msg.content);
+	if(msg.author.id !== env.yourId) return;
+	if(msg.channel.id === env.discordChannel) {
+		if(!msg.content.startsWith('!')) {
+            a.chat(msg.content);
+        } else {
+            for(const i of tags.list) {
+                if(msg.content.startsWith('!' + i)) return a.chat(tags[i]);
+            }
+        }
 	} else if(msg.content.startsWith('!')) {
 		let args = msg.content.split(' ');
 		let cmd = args[0].substr(1);
@@ -33,29 +43,31 @@ bot.on('message', (msg) => {
 			case 'tag':
 				switch (args[0]) {
 					case 'set':
-						if(!tags.list.includes(args[1])) tags.list.push(args[1]];
-						tags[args[1]] = args.slice(1).join(" ");
-						message.channel.send('Successfully updated tag `' + args[1] + '`!');
+						if(!tags.list.includes(args[1])) tags.list.push(args[1]);
+						tags[args[1]] = args.slice(2).join(" ");
+						msg.channel.send('Successfully updated tag `' + args[1] + '`!');
 						fs.writeFileSync('./tags.json', JSON.stringify(tags));
 						break;
 					case 'delete':
-						if(!tags.list.includes(args[1])) return message.channel.send('That tag doesn\'t exist!');
-						tags.list.splice(tags.list.indexOf(args[1]), 1);
-						fs.writeFileSync('./tags.json', JSON.stringify(tags));
+						if(!tags.list.includes(args[1])) return msg.channel.send('That tag doesn\'t exist!');
+                        tags.list.splice(tags.list.indexOf(args[1]), 1);
+                        delete tags[args[1]];
+                        fs.writeFileSync('./tags.json', JSON.stringify(tags));
+                        msg.channel.send('Successfully deleted tag `' + args[1] + '`!');
 						break;
 					case 'list':
-						message.channel.send(`Showing ${tags.list.length} tags:\n\n\`${tags.list.join("\`, \`")}\``);
+						msg.channel.send(`Showing ${tags.list.length} tags:\n\n\`${tags.list.join("\`, \`")}\``);
 						break;
-					except:
-						message.channel.send('Invalid command!');
+					default:
+						msg.channel.send('Invalid command!');
 						break;
 				}
 				break;
-			except:
-				message.channel.send('Invalid command!');
+			default:
+				msg.channel.send('Invalid command!');
 				break;
 		}
 	}
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(env.botToken);

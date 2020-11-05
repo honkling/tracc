e1e74bb0a2293958f3f3e9bc594c5e1ef89dfa4b
untitledgeese <62254220+untitledgeese@users.noreply.github.com>
2020-11-05 10:26:29 -0600

    Implement tags (#4)
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Create tags.json
    
    * Create package.json

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mineflayer = require('mineflayer');
 const Discord = require('discord.js');
+const fs = require('fs');
+const tags = require('./tags.json');
+const envy = require('envy');
+const env = envy();
 const bot = new Discord.Client();
 const a = mineflayer.createBot({
-	host: process.env.SERVER,
-	username: process.env.MINECRAFT_EMAIL,
-	password: process.env.MINECRAFT_PASS
+	host: env.server,
+	username: env.minecraftEmail,
+	password: env.minecraftPass
 });
 
 a.once('spawn', () => {
@@ -13,15 +17,55 @@ a.once('spawn', () => {
 
 a.on('message', (msg) => {
 	console.log(msg.toString());
-	if(msg.toString() === '') return;
 	let message = msg.toString().replace(/(\`)/g, '');
-	if(`\`${message}\`` === '``') return;
-	bot.channels.cache.get(process.env.DISCORD_CHANNEL).send(`\`${message}\``);
+	if(msg.toString() === '' || `\`${message}\`` === '``') return;
+	bot.channels.cache.get(env.discordChannel).send(`\`${message}\``);
 });
 
 bot.on('message', (msg) => {
-	if(msg.author.id !== process.env.YOUR_ID) return;
-	a.chat(msg.content)
+	if(msg.author.id !== env.yourId) return;
+	if(msg.channel.id === env.discordChannel) {
+		if(!msg.content.startsWith('!')) {
+            a.chat(msg.content);
+        } else {
+            for(const i of tags.list) {
+                if(msg.content.startsWith('!' + i)) return a.chat(tags[i]);
+            }
+        }
+	} else if(msg.content.startsWith('!')) {
+		let args = msg.content.split(' ');
+		let cmd = args[0].substr(1);
+		args.shift();
+		// This'll be the minimal thing for commands, I guess. Cant be bothered with a command handler rn.
+		switch (cmd) {
+			case 'tag':
+				switch (args[0]) {
+					case 'set':
+						if(!tags.list.includes(args[1])) tags.list.push(args[1]);
+						tags[args[1]] = args.slice(2).join(" ");
+						msg.channel.send('Successfully updated tag `' + args[1] + '`!');
+						fs.writeFileSync('./tags.json', JSON.stringify(tags));
+						break;
+					case 'delete':
+						if(!tags.list.includes(args[1])) return msg.channel.send('That tag doesn\'t exist!');
+                        tags.list.splice(tags.list.indexOf(args[1]), 1);
+                        delete tags[args[1]];
+                        fs.writeFileSync('./tags.json', JSON.stringify(tags));
+                        msg.channel.send('Successfully deleted tag `' + args[1] + '`!');
+						break;
+					case 'list':
+						msg.channel.send(`Showing ${tags.list.length} tags:\n\n\`${tags.list.join("\`, \`")}\``);
+						break;
+					default:
+						msg.channel.send('Invalid command!');
+						break;
+				}
+				break;
+			default:
+				msg.channel.send('Invalid command!');
+				break;
+		}
+	}
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(env.botToken);

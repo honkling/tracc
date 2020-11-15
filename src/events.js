11a298dce6a32668cf3277292a62f76bc21a2a0a
Goose Go Honk <untitledgeese@gmail.com>
2020-11-15 00:19:11 +0000

    Added server switching! Sneaky deets in README.

---
+++ src/events.js
@@ -0,0 +1,22 @@
+module.exports = (client, bot) => {
+    client.on('spawn', () => {
+        console.log('Minecraft bot spawned!');
+    });
+
+    client.on('kicked', (reason) => {
+        console.log('Minecraft bot kicked from server! Reason: ' + reason);
+        client.connectedToMCServer = false;
+    });
+
+    client.on('end', () => {
+        console.log('Minecraft bot disconnected from server!');
+        client.connectedToMCServer = false;
+    });
+
+    client.on('message', (msg) => {
+	    console.log(msg.toString());
+	    let message = msg.toString().replace(/(\`)/g, '');
+	    if(msg.toString() === '' || `\`${message}\`` === '``') return;
+	    bot.channels.cache.get(process.env.DISCORD_CHANNEL).send(`\`${message}\``);
+    });
+}

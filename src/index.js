f48aca3b3ec9b1591ddcbd115816d092d540f7cd
Goose Go Honk <untitledgeese@gmail.com>
2021-02-11 15:06:32 +0000

    added epics

--- src/index.js
+++ src/index.js
@@ -1,53 +1,66 @@
-const { CommandoClient } = require('discord.js-commando');
-const ClientManager = require('./ClientManager');
-const fs = require('fs');
-const path = require('path');
-const tags = require('./tags.json');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
-const bot = new CommandoClient({
-    owner: process.env.YOUR_ID,
-    commandPrefix: process.env.PREFIX,
-});
-let client = new ClientManager({
-    host: process.env.SERVER,
-    username: process.env.MINECRAFT_EMAIL,
-    password: process.env.MINECRAFT_PASS,
-    version: process.env.VERSION,
-    bot: bot
-});
-bot.client = client;
+const mineflayer = require('mineflayer');
+const { join } = require('path');
+require('dotenv').config({ path: join(__dirname, '../.env') });
 
+let client = mineflayer.createBot({
+	host: process.env.SERVER,
+	username: process.env.EMAIL,
+	password: process.env.PASSWORD,
+});
+const ws = new require('socket.io')(8080, { origins: ["http://localhost:*","http://127.0.0.1:*"], path: "/" });
 
-bot.once('ready', () => {
-    console.log('Discord bot ready!');
-    if(!tags.managers.includes(process.env.YOUR_ID)) {
-        tags.managers.push(process.env.YOUR_ID);
-        fs.writeFileSync('./tags.json', JSON.stringify(tags));
-    }
+const conns = [];
+
+const emitGlobally = (event, data) => conns.forEach(c => c.emit(event, data));
+
+ws.on('connection', (conn) => {
+	conns.push(conn);
+
+	conn.on('disconnect', () => {
+		conns.splice(1, conns.indexOf(conn));
+	});
 });
 
-bot.on('message', (msg) => {
-    if(msg.author.bot) return;
-    if(msg.channel.id !== process.env.DISCORD_CHANNEL) return;
-    if(msg.content.startsWith(process.env.PREFIX)) {
-        const tag = tags.list.filter(x => x === msg.content.split(' ')[0].substr(process.env.PREFIX.length));
-        if(tag === []) return client.chat(msg.content);
-        else if(typeof tags.tags[tag[0]] === 'string') return client.chat(tags.tags[tag[0]]);
-    }
-    client.chat(msg.content);
-});
+const events = (bot) => {
+	bot.on('spawn', () => {
+		console.log('Bot spawned!');
+	});
 
-bot.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['tag', 'Tags'],
-        ['mc', 'Minecraft'],
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands({
-        help: false,
-        unknownCommand: false,
-    })
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+	bot.on('message', (msg) => {
+		console.log(msg.toString());
+		emitGlobally("message", msg);
+		const stripped = msg.toString();
+		if(stripped.startsWith('[AD]')) {
+			let replaced = stripped.replace('[AD]', '');
+			const rank = (replaced.match(/\[[A-Z]{3,8}\]/) || ['default'])[0].replace(/(\[|\])/g, '').toLowerCase();
+			const username = replaced.replace(/\[[A-Z]{3,8}\]\ /, '').replace(/\:/, '').split(' ')[0].replace(/\:/, '');
+			replaced = replaced.replace(/^(.*?)\:\ \/join\ /g, '');
+			const server = replaced.split(' ')[0];
+			const message = replaced.split(' ').slice(1).join(' ');
+			emitGlobally("ad", { rank, username, server, message });
+		} else if(stripped.startsWith('The lobby you are in is shutting down for an automatic restart! Prepare to be kicked.')) {
+			bot.chat(`/find ${bot.username}`);
+		} else if(stripped.replace(/^(.*?)\ /g, '').startsWith(bot.username) && !stripped.startsWith('[AD]')) {
+			emitGlobally("restart", parseInt(stripped.replace(/^(.*?)\ \[Lobby\ /g, '').replace(/\]/g, '')));
+		}
+	});
 
-bot.login(process.env.BOT_TOKEN);
+	bot.on('chat', (username, message, translate, msg) => {
+		if(!msg.toString().startsWith('[AD]'))
+			emitGlobally("chat", { username, message, raw: msg });
+	});
+
+	bot.on('kicked', (reason) => {
+		console.log(`Bot was kicked: ${JSON.parse(reason).text}\nRejoining in 10 seconds.`);
+		setTimeout(() => {
+			client = mineflayer.createBot({
+				host: process.env.SERVER,
+				username: process.env.EMAIL,
+				password: process.env.PASSWORD,
+			});
+			events(client);
+		}, 10000);
+	});
+}
+
+events(client);
